842d475481d70a07cca326b3be4956de05abc836
zhifeng ou <zhifengou@hotmail.com>
2018-10-14 23:51:25 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,20 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+
   end
 
   def edit
+
   end
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
@@ -23,3 +28,4 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 end
+
